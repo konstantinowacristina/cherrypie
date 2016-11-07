3560f3a8f5428d6b4fcc6377d20932452c344913
levashovapolina <polina-levashova@yandex.ru>
2016-11-07 13:10:06 +0300

    Changed name of the context class

--- StudentQueries/Context.cs
+++ StudentQueries/Context.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace StudentQueries
 {
-	class Context : DbContext
+	class OwnContext : DbContext
 	{
 		public DbSet<Student> Students { get; set; }
 		public DbSet<Group> Groups { get; set; }
 
-		public Context() : base("localsql")
+		public OwnContext() : base("localsql")
 		{
 
 		}
